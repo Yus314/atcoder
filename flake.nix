5e30927dd18410ef54ed6990c46f8e93dae3159a
Yus314 <shizhaoyoujie@gmail.com>
2024-06-09 03:29:54 +0900

    can lsp & test

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "Rust development environment with Nix Flakes";
+  #ref https://nixos.wiki/wiki/Rust
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            #clang
+            #llvmPackages_latest.bintools
+            rustup
+          ];
+
+          shellHook = ''
+            exec zsh
+          '';
+        };
+      }
+    );
+}
